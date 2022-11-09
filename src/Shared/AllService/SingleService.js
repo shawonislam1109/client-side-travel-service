57920b3f4d70e01f197957519195d075ae842613
shawonislam1109 <shawonislam1109@gmail.com>
2022-11-09 12:57:52 +0600

    rating point setup

--- src/Shared/AllService/SingleService.js
+++ src/Shared/AllService/SingleService.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
 import { Link } from 'react-router-dom'
 
 const ServiceSingle = ({ service }) => {
-    const { img, name, cost, details, _id } = service
+    const { img, name, cost, details, _id, rating } = service
     return (
         <div className="card card-compact  bg-base-100 shadow-xl my-10 m-10">
-            <figure><img src={img} alt="Shoes" /></figure>
+            <div className='relative'>
+                <figure><img className='rounded-md' src={img} alt="Shoes" /></figure>
+                <div className='flex items-center absolute top-2 right-3'>
+                    <p className='text-xl font-bold '>{rating}  </p>
+                    <FaStar className='text-yellow-500'></FaStar>
+                </div>
+            </div>
             <div className="card-body">
                 <h2 className="card-title font-bold text-yellow-600">{name}</h2>
                 <p className='font-semibold text-slate-800'>{details ? details.slice(0, 100) + `...see more` : 'undefined'}</p>
--- src/Shared/Myservice/MyserviceDetails.js
+++ src/Shared/Myservice/MyserviceDetails.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaStar } from 'react-icons/fa';
 
 const MyserviceDetails = ({ service }) => {
-    const { img, name, cost, details, _id } = service
+    const { img, name, cost, details, _id, rating } = service
     return (
         <div className="card card-compact  bg-base-100 shadow-xl my-10 m-10">
-            <figure><img src={img} alt="Shoes" /></figure>
+            <div className='relative'>
+                <figure><img className='rounded-md' src={img} alt="Shoes" /></figure>
+                <div className='flex items-center absolute top-2 right-3'>
+                    <p className='text-xl font-bold '>{rating}  </p>
+                    <FaStar className='text-yellow-500'></FaStar>
+                </div>
+            </div>
             <div className="card-body">
                 <h2 className="card-title font-bold text-yellow-600">{name}</h2>
                 <p className='font-semibold text-slate-800'>{details ? details.slice(0, 100) + `...see more` : 'undefined'}</p>
@@ -13,6 +20,7 @@ const MyserviceDetails = ({ service }) => {
                 <div className="card-actions justify-end">
                     <Link to={`/SingleService/${_id}`}><button className="btn btn-primary font-bold">Details</button></Link>
                 </div>
+
             </div>
         </div>
     );
